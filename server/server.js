716caca66d09fcf566239501f246905dc28f495d
Mohammed <mohdraza.syed@gmail.com>
2022-08-20 01:17:33 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/api/users");
+const authRoute = require("./routes/api/auth");
+const profileRoute = require("./routes/api/profile");
+const postRoute = require("./routes/api/posts");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/api", (req, res) => res.send("Api running"));
+
+// define routes
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postRoute);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
+connectDB();

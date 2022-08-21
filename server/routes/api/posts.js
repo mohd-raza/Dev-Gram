8927be521144a19e10658c18ebb86cf9fe2becaa
Mohammed <mohdraza.syed@gmail.com>
2022-08-21 14:58:36 +0530

    Post Model

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../../model/User");
+const Post = require("../../model/Post");
+const Profile = require("../../model/Profile");
+
+// @route -> POST api/posts
+// @desc  -> create a post
+// @access-> Private
+
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route -> GET api/posts
-// @desc  -> Test route
-// @access-> Public
-router.get("/", (req, res) => {
-  res.send("Posts Mode");
-});
+// @desc  -> Get all posts
+// @access-> Private
 
 module.exports = router;

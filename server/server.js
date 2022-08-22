458f68ca8beaed3c45e5affc77aff58f76d95893
Mohammed <mohdraza.syed@gmail.com>
2022-08-22 19:27:04 +0530

    Cors resolved

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const userRoute = require("./routes/api/users");
 const authRoute = require("./routes/api/auth");
@@ -9,6 +10,8 @@ const postRoute = require("./routes/api/posts");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json({ extended: false }));
 app.get("/api", (req, res) => res.send("Api running"));
 

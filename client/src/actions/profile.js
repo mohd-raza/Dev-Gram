62a178028cf0360de72bf6de9efefc1cece7739e
Mohammed <mohdraza.syed@gmail.com>
2022-09-11 14:26:08 +0530

    Profile Actions Done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import { useHistory } from "react-router-dom";
 
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -46,7 +46,7 @@ export const createProfile =
       );
 
       if (!edit) {
-        navigate.push("/dashboard");
+        navigate("/dashboard");
       }
     } catch (err) {
       const errors = err.response.data.errors;
@@ -61,3 +61,115 @@ export const createProfile =
       });
     }
   };
+
+// Add experience
+
+export const addExperience = (formData, navigate) => async (dispatch) => {
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/profile/experience",
+      formData
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Experience Added", "success"));
+    navigate("/dashboard");
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Add Education
+
+export const addEducation = (formData, navigate) => async (dispatch) => {
+  try {
+    const res = await axios.post(
+      "http://localhost:5000/api/profile/education",
+      formData
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Education Added", "success"));
+    navigate("/dashboard");
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Delete Experience
+
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(
+      `http://localhost:5000/api/profile/experience/${id}`
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Experience Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// delete Education
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(
+      `http://localhost:5000/api/profile/education/${id}`
+    );
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert("Education Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete account & profile
+
+export const deleteAccount = () => async (dispatch) => {
+  if (window.confirm("Are you sure? this cannot be undone!!")) {
+    try {
+      await axios.delete("/profile");
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  }
+};
